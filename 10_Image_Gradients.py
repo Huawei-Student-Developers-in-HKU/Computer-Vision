335ec4b5bc90296690988ba5a6bf3460cf25192a
Arslanex <enesars716@gmail.com>
2022-12-25 19:55:49 +0300

    Add files via upload

--- 10_Image_Gradients.py
+++ 10_Image_Gradients.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('images/sudoku2.jpeg',0)
+img = cv.imread('images/sudoku2.jpeg', 0)
 laplacian = cv.Laplacian(img,cv.CV_64F)
 sobelx = cv.Sobel(img,cv.CV_64F,1,0,ksize=5)
 sobely = cv.Sobel(img,cv.CV_64F,0,1,ksize=5)
